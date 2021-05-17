62d6bd9df86d6bae22a88a3df5e9adfb5291d562
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-05-17 20:02:11 -0300

    initial commit

--- Aula 6/aula 6.R
+++ Aula 6/aula 6.R
@@ -1,7 +1,24 @@
-############################################################
-#######  #######
-############################################################
+#################################
+####### Usando pipes no R #######
+#################################
 
 getwd()
-setwd('C:/Users/lucas.lima/Documents/Arquivos RStudio/Aula 5')
+setwd('C:/Users/lucas.lima/Documents/Arquivos RStudio/primeiros-passos-r/Aula 6')
 getwd()
+
+library(tidyverse)
+install.packages('babynames')
+library(babynames)
+
+## Exemplo 01
+
+# Sem o pipe '%>%'
+diamonds = ggplot2::diamonds
+diamonds2 = mutate(diamonds, price_per_carat = price / carat)
+
+# Utilizando o pipe '%>%'
+diamonds2 = diamonds %>%
+  mutate(price_per_carat = price / carat)
+
+## Exemplo 02
+babynames
